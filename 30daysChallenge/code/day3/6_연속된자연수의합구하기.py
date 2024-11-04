748fe9c95ff805ff58a4643852566ac2d55516c5
sjHong645 <ixdos1111@gmail.com>
2024-11-04 18:48:25 +0900

    Day3 자료구조 부분 정리 진행 중

---
+++ 30daysChallenge/code/day3/6_연속된자연수의합구하기.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+count = 1; start_index = 1; end_index = 1; sum = 1
+
+while end_index != n : 
+    
+    if sum > n : 
+        sum -= start_index
+        start_index += 1
+        
+    elif sum < n : 
+        end_index += 1
+        sum += end_index
+    
+    else : # sum == n 
+        count += 1
+        end_index += 1
+        sum += end_index 
+
+print(count)
