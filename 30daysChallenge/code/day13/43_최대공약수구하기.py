8bd80da8eb6e9e4186170063747534e0d31d9a70
sjHong645 <ixdos1111@gmail.com>
2024-11-10 15:04:38 +0900

    Day 13 정수론 부분 정리 완료

---
+++ 30daysChallenge/code/day13/43_최대공약수구하기.py
@@ -0,0 +1,16 @@
+def gcd(a, b) : 
+    if b == 0 : 
+        return a 
+    
+    else :
+        gcd(b, a % b)
+
+a, b = map(int, input().split())
+
+result = gcd(a, b)
+
+while result > 0 : 
+    print(1, end = '')
+    result -= 1
+
+    
