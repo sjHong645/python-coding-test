bc74f8217f5069aca24fc1fd2299e57703733e86
sjHong645 <ixdos1111@gmail.com>
2024-12-05 16:58:11 +0900

    Day20 트리 정리 완료

---
+++ 30daysChallenge_part2/code/day20/68_리프노드의개수구하기.py
@@ -0,0 +1,39 @@
+N = int(input())
+visited = [False] * (N)
+tree = [[] for _ in range(N)]
+answer = 0
+p = list(map(int, input().split())) 
+
+# 트리 데이터 저장
+for i in range(N) : 
+    
+    if p[i] != -1 : 
+        tree[i].append(p[i])
+        tree[p[i]].append(i)
+        
+    else : 
+        root = i
+
+# DFS 탐색 메소드
+def DFS(number) : 
+    
+    global answer 
+    visited[number] = True 
+    child_node = 0
+    
+    for i in tree[number] : 
+        if not visited[i] and i != deleteNode : 
+            child_node += 1
+            DFS(i)
+            
+    if child_node == 0 :
+        answer += 1 
+
+deleteNode = int(input())
+
+if deleteNode == root : # 루트노드가 삭제될 경우
+    print(0)
+    
+else :
+    DFS(root)
+    print(answer)
